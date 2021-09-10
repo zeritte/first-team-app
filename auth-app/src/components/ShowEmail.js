eb7947555395d2a0762edf6b1de6cb89b5c4a627
MAkifUNLU <77721632+MAkifUNLU@users.noreply.github.com>
2021-09-10 22:07:18 +0300

    showemail file created

---
+++ auth-app/src/components/ShowEmail.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default () => {
+
+    return (
+        <View>
+            <Text style={styles.textEmail}>
+   
+            </Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    textEmail: {
+
+    } 
+});
+  
