2468f97f566b336f5434f77982fef22f00f621be
MAkifUNLU <77721632+MAkifUNLU@users.noreply.github.com>
2021-09-11 14:40:15 +0300

    Log out button added

--- auth-app/src/screens/ProfileScreen.js
+++ auth-app/src/screens/ProfileScreen.js
@@ -1,11 +1,26 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
+import ShowEmail from "../components/ShowEmail";
 import TaskCreator from "../components/TaskCreator";
 
 const ProfileScreen = () => {
+
+  onSubmit = async () => {
+    try {
+      await AsyncStorage.removeItem("key_mail")
+      navigate('Register')
+    }
+    catch (e){
+      console.log(e)
+    }
+  }
+
   return (
     <View style={styles.container}>
       <TaskCreator />
+      <ShowEmail />
+      <Button title='LOG OUT' onPress={onSubmit} />
     </View>
   );
 };
