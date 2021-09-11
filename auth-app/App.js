d2f4ae006e3c8254f691a1f659a723b294837298
MAkifUNLU <77721632+MAkifUNLU@users.noreply.github.com>
2021-09-11 17:32:26 +0300

    Logged in check done

--- auth-app/App.js
+++ auth-app/App.js
@@ -1,21 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import LoginScreen from "./src/screens/LoginScreen";
 import ProfileScreen from "./src/screens/ProfileScreen";
 import RegisterScreen from "./src/screens/RegisterScreen";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Stack = createNativeStackNavigator();
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const checkIsLoggedIn = async () => {
+    const keys = await AsyncStorage.getAllKeys();
+    const value = await AsyncStorage.getItem('key_mail')
+
+    if (keys.includes(value)) {
+      setIsLoggedIn(true)
+    }  else {
+      setIsLoggedIn(false)
+    }
+    // local storage a bak
+    // email kayitli mi diye
+    // kontrol et
+    // eger kayitli email varsa,
+    // setIsLoggedIn(true)
+  };
+
+  useEffect(() => {
+    checkIsLoggedIn();
+  }, []);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-      </Stack.Navigator>
+      {!isLoggedIn ? (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Register" component={RegisterScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator>
+      ) : (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
