b99091524f1cf52eb2e6042804e82b99e2c30cb7
MAkifUNLU <77721632+MAkifUNLU@users.noreply.github.com>
2021-09-13 12:54:12 +0300

    Email display

--- auth-app/src/components/ShowEmail.js
+++ auth-app/src/components/ShowEmail.js
@@ -1,12 +1,24 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useAsyncStorage } from "@react-native-async-storage/async-storage";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default () => {
+  const [value, setValue] = useState("");
+  const { getItem } = useAsyncStorage("@email_key");
+
+  useEffect(() => {
+    writeEmail();
+  }, []);
+
+  const writeEmail = async () => {
+    const item = await getItem();
+    setValue(item);
+  };
 
     return (
-        <View>
+    <View >
             <Text style={styles.textEmail}>
-   
+          {value}
             </Text>
         </View>
     );
