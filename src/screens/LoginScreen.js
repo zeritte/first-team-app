31f4375c83537de42cc0259dd4e7aab334e7ee9a
zeritte <zeritte@gmail.com>
2021-09-19 17:04:24 +0200

    eslint configs

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -3,14 +3,12 @@ import { StyleSheet, View } from "react-native";
 import AuthForm from "../components/AuthForm";
 import NavLink from "../components/NavLink";
 
-const LoginScreen = () => {
-  return (
-    <View style={styles.container}>
-      <AuthForm headerText="Login to Account" submitButtonText="LOGIN" />
-      <NavLink text="Don't have an account? Register instead." routeName="Register" />
-    </View>
-  );
-};
+const LoginScreen = () => (
+  <View style={styles.container}>
+    <AuthForm headerText="Login to Account" submitButtonText="LOGIN" />
+    <NavLink text="Don't have an account? Register instead." routeName="Register" />
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
