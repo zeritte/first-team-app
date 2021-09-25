a2cd76a1d900632b8052a119d7473b4db49f99b2
hseyyidkaplan <hseyyidkaplan@gmail.com>
2021-09-25 18:23:45 +0300

    functions divided into basic parts

--- src/textValidator.js
+++ src/textValidator.js
@@ -9,20 +9,28 @@ const countLetter = (string, char) => {
   return count;
 };
 
-export const emailValidate = (email) => {
-  // '@' isareti ve 2 adet nokta icermeli
-  if (countLetter(email, '@') === 1 && countLetter(email, '.') >= 2) return false;
-  return "Your email should include one '@' sign and two dots('.')!";
+// @?? aşağıdaki eslint hatası neden?
+// eslint-disable-next-line arrow-body-style
+export const countAt = (email) => {
+  return countLetter(email, '@') === 1;
 };
 
-export const nameValidate = (name) => {
-  // isim 5 karakterden buyuk ve bosluksuz olmali
-  if (name.length >= 5 && !name.includes(' ')) return false;
-  return "Your name should be 5 or more characters and doesn't include space(' ')";
+// eslint-disable-next-line arrow-body-style
+export const countDot = (email) => {
+  return countLetter(email, '.') >= 2;
 };
 
-export const passwordValidate = (password) => {
-  // sifre en az 6 karakter olmali
-  if (password.length >= 6) return false;
-  return 'Your password should be at least 6 characters!';
+// eslint-disable-next-line arrow-body-style
+export const nameLength = (name) => {
+  return name.length >= 5;
+};
+
+// eslint-disable-next-line arrow-body-style
+export const nameSpace = (name) => {
+  return !name.includes(' ');
+};
+
+// eslint-disable-next-line arrow-body-style
+export const passwordLength = (password) => {
+  return password.length >= 6;
 };
