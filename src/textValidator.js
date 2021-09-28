57c0dfa0f89aa3b4d2dad042adfcfaa5f906e6ed
hseyyidkaplan <hseyyidkaplan@gmail.com>
2021-09-28 22:39:13 +0300

    these 3 func. moved temporarily, will be removed soon

--- src/textValidator.js
+++ src/textValidator.js
@@ -1,5 +1,23 @@
 // NOTE: screenler componentlar sadece gostersin hesap kitap yapmasin
 
+export const emailValidate = (email) => {
+  // '@' isareti ve 2 adet nokta icermeli
+  if (countAt(email) && countDot(email)) return false;
+  return "Your email should include one '@' sign and two dots('.')!";
+};
+
+export const nameValidate = (name) => {
+  // isim 5 karakterden buyuk ve bosluksuz olmali
+  if (nameLength(name) && nameSpace(name)) return false;
+  return "Your name should be 5 or more characters and doesn't include space(' ')";
+};
+
+export const passwordValidate = (password) => {
+  // sifre en az 6 karakter olmali
+  if (passwordLength(password)) return false;
+  return 'Your password should be at least 6 characters!';
+};
+
 const countLetter = (string, char) => {
   let count = 0;
   const charArr = string.split('');
